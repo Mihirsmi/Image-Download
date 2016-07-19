a8560384e77778d88c11d19c0cabdeed1b353d9e
Mihirsmi <mihirsmi404@gmail.com>
2016-07-19 12:00:33 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,17 @@
 import urllib2
 import os
-try:
-    url = '201501300.jpg'
-    file = open(url,'wb')
-    file.write(urllib2.urlopen('https://ecampus.daiict.ac.in/webapp/intranet/StudentPhotos/'+url).read())
-    file.close()
-    print 'File created may be error in URL'
-except:
-    os.remove(url)
-    print 'Error in URL but removed'
+url = 'https://ecampus.daiict.ac.in/webapp/intranet/StudentPhotos/'
+for i in range(1,462):
+    try:
+        filename = '201501'+str(i).zfill(3)+'.jpg'
+        '''
+        file = open(url,'wb')
+        file.write(urllib2.urlopen(url+filename).read())
+        file.close()
+        print 'File created may be error in URL'
+        '''
+        print filename
+    except:
+        os.remove(filename)
+        print 'Error in URL but removed'
+print 'Sucess in downloading'
