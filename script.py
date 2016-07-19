947cab4c0fadf91915f9d7606d501003e4284cc0
Mihirsmi <mihirsmi404@gmail.com>
2016-07-20 02:03:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,14 @@
 import urllib2
 import os
 url = 'https://ecampus.daiict.ac.in/webapp/intranet/StudentPhotos/'
-for i in range(1,462):
+#import pdb;pdb.set_trace()
+for i in range(1,50):
     try:
         filename = '201501'+str(i).zfill(3)+'.jpg'
-        '''
-        file = open(url,'wb')
+        file = open(filename,'wb')
         file.write(urllib2.urlopen(url+filename).read())
         file.close()
         print 'File created may be error in URL'
-        '''
         print filename
     except:
         os.remove(filename)
